e8452d3fba8573bb1e84567078d042cdb883c9c4
fllantada <franciscollantada@gmail.com>
2022-10-27 12:14:09 -0300

    me traje los juguetes del chat

---
+++ src/controller/users.js
@@ -0,0 +1,35 @@
+let users = [];
+
+//Join User to CHat
+
+const addUser = (id, username, room) => {
+  const user = {
+    id,
+    username,
+    room,
+  };
+
+  users.push(user);
+  console.log('CLIENTES ACTIVOS');
+  console.log(users)
+};
+
+const removeUser = (id) => {
+  users = users.filter((aUser) => aUser.id !== id);
+};
+
+const getCurrentUser = (id) => {
+  return users.find((aUser) => aUser.id === id);
+};
+
+const getRoomUsers = (room) => {
+  return users.filter((aUser) => aUser.room === room);
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getCurrentUser,
+  getCurrentUser,
+  getRoomUsers,
+};
