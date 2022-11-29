830d364e7f577b441f4aa956042f8e86a144c8e3
fllantada <franciscollantada@gmail.com>
2022-11-29 02:10:41 -0300

    2da PRe entrega

--- src/app/percistence/MongoSaver.ts
+++ src/app/percistence/MongoSaver.ts
@@ -1,25 +1,62 @@
 import IPersistenceRepository from "../Interfaces/IPersistenceRepository";
+import * as mongoDb from "mongodb";
+import { FileSaverObject } from "../Interfaces/IFileSaverObject";
 
-export class MongoSaver implements IPersistenceRepository {
-  private db: any;
-  private collection: string;
+export default class MongoSaver implements IPersistenceRepository {
+  private db: mongoDb.Db;
+  private client: mongoDb.MongoClient;
+  private collection: mongoDb.Collection;
+  private collectionName: string;
 
-  constructor(collection: string) {}
+  constructor(
+    collectionName: string,
+    url: string = "mongodb://localhost:27017"
+  ) {
+    this.collectionName = collectionName;
+    this.client = new mongoDb.MongoClient(url, {});
 
-  async getAll(): Promise<any[]> {
-    return [];
+    this.connect();
+  }
+  async connect() {
+    if (this.db) {
+      return;
+    }
+    await this.client.connect();
+    const dbName = "myproject";
+    this.db = this.client.db(dbName);
+    this.collection = this.db.collection(this.collectionName);
+    console.log(this.collection);
+  }
+
+  disconnect() {
+    this.client.close();
+  }
+
+  async getAll(): Promise<FileSaverObject[]> {
+    const items = await this.collection.find({}).toArray();
+    return items.map((item: any) => ({ id: item._id, ...item }));
   }
 
   async getById(id: string): Promise<false | any> {
-    return false;
+    const item = await this.collection.find({ _id: id }).toArray();
+    if (item.length === 0) {
+      return false;
+    }
+    return item[0];
   }
   create(item: any): string {
+    const id = this.collection.insertOne(item);
     return "OK";
   }
   async edit(id: string, item: any): Promise<any | false> {
-    return false;
+    const itemToEdit = await this.collection.find({ _id: id }).toArray();
+    if (itemToEdit.length === 0) {
+      return false;
+    }
+    return itemToEdit[0];
   }
   async delete(id: string): Promise<boolean> {
-    return false;
+    const result = await this.collection.deleteOne({ _id: id });
+    return result.deletedCount === 1;
   }
 }
