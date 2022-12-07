6a3d53b958a00dedf72dae3e6f4f1a1fb7691a16
fllantada <franciscollantada@gmail.com>
2022-12-07 15:24:22 -0300

    socket modified

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,25 +1,44 @@
 import { Server as httpServer } from "http";
 import { Server as ioServer } from "socket.io";
 import { Socket as socketType } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
+import { denormalize, normalize, schema } from "normalizr";
+import IPersistenceRepository from "../app/Interfaces/IPersistenceRepository";
 import moment from "moment";
 import productApp from "../app/ProductApp";
 
 type Message = {
-  user: string;
+  id: string;
+  author: Author;
+  text: string;
   date: string;
-  message: string;
 };
-type user = {
+
+type Author = {
   id: string;
   email: string;
+  nombre: string;
+  apellido: string;
+  edad: number;
+  alias: string;
+  avatar: string;
 };
+
+const author = new schema.Entity("authors", {});
+
+const message = new schema.Entity("messages", { author });
+
+const finalSchema = new schema.Array(message);
 
 export class SocketClass {
   private io: ioServer;
   private messages: Message[] = [];
-  private users: user[] = [];
+  private authors: Author[] = [];
 
-  constructor(app: httpServer) {
+  constructor(
+    app: httpServer,
+    private persistenceRepository: IPersistenceRepository
+  ) {
     this.connectIoToServer(app);
     this.handleEvents();
   }
@@ -32,7 +51,9 @@ export class SocketClass {
       socket.emit("allProducts", productApp.getAll());
 
       socket.on("login", (data) => {
-        this.users.push({ id: socket.id, email: data });
+        const author = this.createAuthor(data);
+
+        this.authors.push(author);
         socket.emit("loggedUser", data);
         socket.emit("newMessage", [
           {
@@ -52,30 +73,52 @@ export class SocketClass {
 
       socket.on("chatMessage", (data: string) => {
         const message = this.createMessage(socket.id, data);
+        if (!message) return;
         this.messages.push(message);
+        this.persistenceRepository.create(message);
         this.io.emit("newMessage", this.messages);
       });
 
       socket.on("disconnect", () => {
-        this.users = this.users.filter((user) => user.id !== socket.id);
+        this.authors = this.authors.filter((user) => user.id !== socket.id);
       });
     });
   }
-  createMessage(socketId: string, data: string): Message {
-    const user = this.users.find((user) => user.id === socketId);
+
+  createAuthor(data: any): Author {
+    return {
+      id: uuidv4(),
+      email: data.email,
+      nombre: data?.nombre,
+      apellido: data?.apellido,
+      edad: data?.edad,
+      alias: data?.alias,
+      avatar: data?.avatar,
+    };
+  }
+
+  createMessage(socketId: string, data: string): Message | undefined {
+    const user: Author | undefined = this.authors.find(
+      (user) => user.id === socketId
+    );
+
     if (user) {
       return {
-        user: user?.email,
+        author: user,
+        id: uuidv4(),
+        text: data,
         date: moment().format("DD/MM/YYYY HH:mm:ss"),
-        message: data,
-      };
-    } else {
-      return {
-        user: "Anonimo",
-        date: moment().format("DD/MM/YYYY HH:mm:ss"),
-        message: data,
       };
     }
+  }
+  async getAllNormalized() {
+    const data = this.persistenceRepository.getAll();
+    return normalize(data, finalSchema);
+  }
+
+  async getAllDenormalized() {
+    const data = await this.getAllNormalized();
+    return denormalize(data.result, finalSchema, data.entities);
   }
 
   public getIo(): ioServer {
