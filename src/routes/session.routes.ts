668b7edbc1fc765971d8c9b97bb75d1da43aed21
fllantada <franciscollantada@gmail.com>
2022-12-15 05:48:15 -0300

    setSession ROute adeed

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -5,9 +5,10 @@ const session = Router();
 
 session.get("/set-session", (req: Request, res: Response) => {
   // Almacenar información en la sesión del usuario
-  if (req.session.userId && req.session.userName) {
-    req.session.userId = 123;
+
+  if (req.session) {
     req.session.userName = "john";
+    req.session.userId = 123;
   }
 
   // Enviar una respuesta
