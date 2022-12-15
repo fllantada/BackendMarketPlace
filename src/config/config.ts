67ac278ee886f12a2c02ba8bc960ee0a6f2276ce
fllantada <franciscollantada@gmail.com>
2022-12-15 02:29:06 -0300

    config file added

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,15 @@
 export const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "mysecret",
+
+  sessionConfig: {
+    secret: process.env.SESSION_SECRET || "mysecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+      secure: false,
+    },
+  },
 };
--- src/services/server.ts
+++ src/services/server.ts
@@ -5,22 +5,14 @@ import path from "path";
 import { createServer } from "http";
 import User from "../auth/isAdmin";
 import session from "express-session";
+import { config } from "src/config/config";
 
-const sessionConfig = session({
-  secret: "secret",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24,
-    httpOnly: true,
-    secure: false,
-  },
-});
+const sessionFunc = session(config.sessionConfig);
 
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(sessionConfig);
+app.use(sessionFunc);
 
 //user Status for protected Routes
 
